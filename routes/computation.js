576939ed10c27f3c7dcd582e3323e56de6a2ead9
saida-reddy <108296645+saida-reddy@users.noreply.github.com>
2022-10-26 16:46:26 -0500

    modifications

---
+++ routes/computation.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var value;
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+     value =Number(search_params.get("x"))
+    if(value == 0)
+    value=Math.random()
+    res.write("Computes the values for Math.abs and math.sin function."+"\n")
+    res.write('Math.cos() applied to '+value+" is "+Math.cos(value,value)+"\n");
+    res.write('Math.asin()  applied to '+value+" is "+Math.asin(value)+"\n");
+    res.write('Math.asinh() applied to '+value+" is "+Math.asinh(value)+"\n");
+ });
+
+module.exports = router;
